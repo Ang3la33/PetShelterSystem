6c9f98954cdc762563be3622efc2947c5df74de8
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-01-29 21:41:38 -0330

    Updated Pet class to implement forstering and a waitlist by creating methods for fostering, returning from foster care, and setting on a waitlist. Updated toString method to list foster status if any and list if pet is currently waitlisted

--- src/main/java/com/petshelter/Pet.java
+++ src/main/java/com/petshelter/Pet.java
@@ -7,6 +7,8 @@ public class Pet {
     private String breed;
     private int age;
     private boolean isAdopted;
+    private boolean isFostered;
+    private boolean isOnWaitlist;
 
     public Pet(String name, String species, String breed, int age) {
         this.name = name;
@@ -14,6 +16,8 @@ public class Pet {
         this.breed = breed;
         this.age = age;
         this.isAdopted = false;
+        this.isFostered = false;
+        this.isOnWaitlist = false;
     }
 
     public String getName() {
@@ -52,17 +56,54 @@ public class Pet {
         return isAdopted;
     }
 
+    public boolean isFostered() {
+        return isFostered;
+    }
+
+    public boolean isOnWaitlist() {
+        return isOnWaitlist;
+    }
+
     public void adopt() {
-        if (!isAdopted) {
+        if (!isAdopted() && !isFostered) {
             isAdopted = true;
             System.out.println(name + " has been adopted!");
+        } else if (isFostered){
+            System.out.println(name + " is currently in foster care and cannot be adopted.");
         } else {
             System.out.println(name + " is already adopted.");
         }
     }
 
+    public void foster() {
+        if (!isAdopted() && !isFostered) {
+            isFostered = true;
+            System.out.println(name + " is now in foster care.");
+        } else if (isAdopted) {
+            System.out.println(name + " has already been adopted and cannot be fostered.");
+        } else {
+            System.out.println(name + " is already in foster care.");
+        }
+    }
+
+    public void returnFromFoster() {
+        if (isFostered) {
+            isFostered = false;
+            System.out.println(name + " has been returned from foster care.");
+        } else {
+            System.out.println(name + " is not currently in foster care.");
+        }
+    }
+
+    public void setOnWaitlist(boolean status) {
+        this.isOnWaitlist = status;
+    }
+
+
+
     @Override
     public String toString() {
-        return name + " ( " + species + ", " + breed + ", " + age + " )" + (isAdopted ? "[Adopted]" : "[Available]");
+        return name + " ( " + species + ", " + breed + ", " + age + " )" + (isAdopted ? "[Adopted]" : isFostered ? "[Fostered]" : "[Available]") +
+                (isOnWaitlist ? " - Waitlisted" : "");
     }
 }
