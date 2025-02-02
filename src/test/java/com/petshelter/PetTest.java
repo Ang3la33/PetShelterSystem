32a724031d38c913c05eeee7c6a36e3d962395ee
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-02-02 17:56:09 -0330

    Added unit tests for Pet class

--- src/test/java/com/petshelter/PetTest.java
+++ src/test/java/com/petshelter/PetTest.java
@@ -1,4 +1,39 @@
 package com.petshelter;
 
+import com.petshelter.model.Pet;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class PetTest {
+
+    private Pet pet;
+
+    @BeforeEach
+    void setUp() {
+        pet = new Pet("MacGyver","Dog","Rottweiler", 9);
+    }
+
+    @Test
+    void testInitialPetState() {
+        assertEquals("MacGyver",pet.getName());
+        assertEquals("Dog", pet.getSpecies());
+        assertEquals("Rottweiler", pet.getBreed());
+        assertEquals(9, pet.getAge());
+        assertFalse(pet.isAdopted());
+        assertFalse(pet.isFostered());
+    }
+
+    @Test
+    void testAdoptPet() {
+        pet.setAdopted(true);
+        assertTrue(pet.isAdopted());
+    }
+
+    @Test
+    void testFosterPet() {
+        pet.setFostered(true);
+        assertTrue(pet.isFostered());
+    }
+
 }
