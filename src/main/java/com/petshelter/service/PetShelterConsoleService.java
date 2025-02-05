08e59aeeba5055d9f8377a4cbd292ac69e70fb31
Angela Flynn <156027242+Ang3la33@users.noreply.github.com>
2025-02-05 00:19:28 -0330

    Initialized constructor for PetShelterConsoleService class

--- src/main/java/com/petshelter/service/PetShelterConsoleService.java
+++ src/main/java/com/petshelter/service/PetShelterConsoleService.java
@@ -8,6 +8,16 @@ import java.util.Scanner;
 
 public class PetShelterConsoleService {
 
+    private Scanner scanner;
+    private PetShelter petShelter;
+    private PetShelterService petShelterService;
+
+    public PetShelterConsoleService(PetShelter petShelter, PetShelterService petShelterService) {
+        this.scanner = new Scanner(System.in);
+        this.petShelter = petShelter;
+        this.petShelterService = petShelterService;
+    }
+
 
 
 }
